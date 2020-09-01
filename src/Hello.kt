ae8fefd0b0b9d054b36de625d39dd609f473011b
Chandan Kumar Mandal <chankruze@gmail.com>
2020-09-01 08:08:53 +0530

    Adds if and when experiments

--- src/Hello.kt
+++ src/Hello.kt
@@ -4,6 +4,7 @@ fun printPattern(char: Char = '*', number: Int = 5) {
     repeat(number) {
         print(char)
     }
+    println()
 }
 
 fun main() {
@@ -11,4 +12,18 @@ fun main() {
     val y = 2
     println("x + y = ${sum(x, y)}")
     printPattern(number = 7)
+
+    // if
+    val mark = 98
+    val remark = if (mark > 80) "Well done !" else "Try harder !"
+    println("Your mark is ${mark}, $remark")
+
+    // when
+    val newMark = 45
+    val newRemark = when {
+        newMark < 50 -> "You should study more !"
+        newMark in 50..80 -> "Not bad !"
+        else -> "Excellent work !"
+    }
+    println("Your new mark is ${newMark}, $newRemark")
 }

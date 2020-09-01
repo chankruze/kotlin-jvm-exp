e6f0c578d55a57aaacdec655e791b333663bf3f9
Chandan Kumar Mandal <chankruze@gmail.com>
2020-09-01 08:46:29 +0530

    Updates when & Adds for loop

--- src/Hello.kt
+++ src/Hello.kt
@@ -26,4 +26,60 @@ fun main() {
         else -> "Excellent work !"
     }
     println("Your new mark is ${newMark}, $newRemark")
+
+    fun reply(input: String) = when (input) {
+        "y", "yes" -> "I'm glad you agree"
+        "n", "no" -> "Sorry to hear that"
+        else -> "I don't understand"
+    }
+
+    println(reply("yes"))
+    println(reply("n"))
+    println(reply("fuck"))
+
+    // when with objects
+//    when (val pet = getMyPet()) {
+//        is Cato -> pet.meow() // smart casting
+//        is Dogo -> pet.woof() // smart casting
+//    }
+
+    // loops
+    // while, do-while same as java
+
+    // for with list
+    val list = listOf("a", "b", "c")
+    for (s in list)
+        println(s)
+
+    for ((index, elem) in list.withIndex())
+        println("Index = $index, Element = $elem")
+
+    // for with map
+    val map = mapOf(1 to "one", 2 to "two", 3 to "three")
+    for ((key, value) in map)
+        println("Key = $key, Val = $value")
+
+    // for with range (upper bound inclusive)
+    for (i in 1..9)
+        if (i % 2 == 0) println(i)
+
+    // for with until (upper bound exclusive)
+    for (i in 1 until 9)
+        print(i)
+    println()
+
+    // for with range + step (up)
+    for (i in 1..9 step 2)
+        print(i)
+    println()
+
+    // for with step (down)
+    for (i in 9 downTo 1 step 2)
+        print(i)
+    println()
+
+    // iterating a string
+    for (c in "test")
+        print(c + 1)
+    println()
 }
